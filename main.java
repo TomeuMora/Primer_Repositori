5935c4847109d74f431e451b4598243f77038ddc
TomeuMora <tmora307@alumnes.politecnicllevant.cat>
2023-11-02 12:56:13 +0100

    nova insercio

--- main.java
+++ main.java
@@ -1,19 +1,3 @@
 
 import java.util.Scanner;
 
-public class proves {
-     public static double suma(double num1, double num2) {
-            return num1 + num2;
-        }
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Introduce el primer numero: ");
-            double i = scanner.nextDouble();
-            System.out.print("Introduce el segon numero: ");
-            double j = scanner.nextDouble();
-
-            double a = suma(i, j);
-
-            System.out.println("El total de la suma es : " + a);
-        }
-    }
